19ffa7b5c818ef29460ce9012927484483c593db
MathiasLux <lux19959@spengergasse.at>
2023-09-28 21:37:20 +0200

    Initial commit

---
+++ src/main/java/at/spengergasse/vergleichsportal/VergleichsportalApplication.java
@@ -0,0 +1,15 @@
+package at.spengergasse.vergleichsportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VergleichsportalApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(VergleichsportalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/vergleichsportal/VergleichsportalApplicationTests.java
@@ -0,0 +1,15 @@
+package at.spengergasse.vergleichsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VergleichsportalApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
